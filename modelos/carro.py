5932dd389ca37212bcf075d7bb15cf081ead5873
Josué Junior <59489348+josuejro@users.noreply.github.com>
2026-02-25 18:35:25 -0300

    definição das classes

---
+++ modelos/carro.py
@@ -0,0 +1,7 @@
+class Carro:
+    def __init__(self, placa, marca, modelo, preco_diaria):
+        self.placa = placa
+        self.marca = marca
+        self.modelo = modelo
+        self.preco_diaria = preco_diaria
+        self.disponivel = True
---
+++ modelos/cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+    def __init__(self, nome, data_nasc, cpf, cnh, cartao_cred):
+        self.nome = nome
+        self.data_nasc = data_nasc
+        self.cpf = cpf
+        self.cnh = cnh
+        self.cartao_cred = cartao_cred
+        
